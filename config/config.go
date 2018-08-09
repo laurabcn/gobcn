e8325ffdaca421a978851edf0534ad490fa106cf
Félix Carpena <felix.carpena@ulabox.com>
2018-08-09 10:43:09 +0200

    Refactor

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+// NewDBConnection returns initialized sql.DB
+func NewDBConnection() (*sql.DB, error) {
+	user := getEnvWithDefault("DB_USER", "root")
+	password := getEnvWithDefault("DB_PASSWORD", "root")
+	host := getEnvWithDefault("DB_HOST", "localhost")
+	port := getEnvWithDefault("DB_PORT", "8082")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/ddd_sample?parseTime=true", user, password, host, port)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func getEnvWithDefault(name, def string) string {
+	env := os.Getenv(name)
+	if len(env) != 0 {
+		return env
+	}
+	return def
+}
